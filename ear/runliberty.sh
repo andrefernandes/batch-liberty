353486fc9052ccce6e35a7c0b4a74b1965a29593
andre <goroh@hotmail.com>
2014-09-23 22:37:40 -0300

    fix jackson error run script for IntelliJ

--- ear/runliberty.sh
+++ ear/runliberty.sh
@@ -1,3 +1,14 @@
+#!/bin/sh
+#
+# Normally "mvn liberty:start-server" would be enough,
+# but this script suits better in a run configuration for
+# IntelliJ (we need it to stay in foreground).
+# The copy operations reproduce the mvn task behaviour.
+#
+WLPDIR=./target/liberty/wlp/usr/servers/defaultServer
+if [ ! -d "$WLPDIR" ]; then
+ mvn liberty:create-server
+fi
 cp ./src/test/resources/server.xml ./target/liberty/wlp/usr/servers/defaultServer/
 cp ./src/test/resources/bootstrap.properties ./target/liberty/wlp/usr/servers/defaultServer/
 cp ./src/test/resources/jvm.options ./target/liberty/wlp/usr/servers/defaultServer/
